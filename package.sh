2ed6dead36bdfbe54aa19284f3de9b46132d6bb3
actboy <actboy@actboys-MacBook-Pro.local>
2023-03-11 11:20:32 +0800

    打包

--- package.sh
+++ package.sh
@@ -18,5 +18,5 @@ tar --exclude=./ui/node_modules --exclude=.idea --exclude=./ui/src --exclude=./u
 --exclude=./ui/*config*  --exclude=__pycache__ --exclude=chat.tar.gz  --exclude=.idea  \
 --exclude=./ui/jest.setup.ts  --exclude=./ui/package.json  --exclude=./ui/yarn.lock \
 --exclude=.gitigore --exclude=.git \
--czvf chat.tar.gz .
-scp -i ~/Documents/keys/jarlyn.pem chat.tar.gz ec2-user@100.25.222.164:/home/ec2-user/
+-czvf chat_app_v1.0.0.tar.gz .
+scp -i ~/Documents/keys/jarlyn.pem chat_app_v1.0.0.tar.gz ec2-user@100.25.222.164:/home/ec2-user/chat_app/v1.0.0/
